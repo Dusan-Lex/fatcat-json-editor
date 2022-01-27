eab1ec1b3a918e7c7b4c55df3da49fc860e66dda
Dusan-Lex <dusanlexmilosavljevic@gmail.com>
2022-01-27 21:49:00 +0100

    overall project setup with styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import BaseStyles from './BaseStyles';
 
 ReactDOM.render(
 	<React.StrictMode>
+		<BaseStyles />
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
---
+++ src/shered/styles.ts
@@ -0,0 +1,18 @@
+import { css } from 'styled-components';
+
+export const color = {
+	primaryLight:	'#52D6F4',
+	primaryDark:	'#408697',
+	secondaryLight:	'#8D8C8A',
+	secondaryDark:	'#363538',
+	tertiary: '#F6F6F6',
+	error: '#f62020',
+};
+
+export const mixin = {
+	center: css`
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	`,
+};
